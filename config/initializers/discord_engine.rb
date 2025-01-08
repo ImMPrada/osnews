035471ed7c750219e1ef929f1cba25240c4c10db
immprada <im.mprada@gmail.com>
2025-01-08 15:26:29 -0500

    setup

---
+++ config/initializers/discord_engine.rb
@@ -0,0 +1,11 @@
+DiscordEngine.public_key = Rails.application.credentials.discord_application.public_key
+DiscordEngine.bot_token = Rails.application.credentials.discord_application.bot_token
+DiscordEngine.application_id = Rails.application.credentials.discord_application.id
+
+DiscordEngine.resolvers = [
+  'Discord::Interactions::Resolvers::Connect',
+  'Discord::Interactions::Resolvers::Message',
+  'Discord::Interactions::Resolvers::RevealMessage'
+]
+
+DiscordEngine.guild_verification = ->(guild) { ::Interface.discord_guild.find_by(external_id: guild.id).present? }
