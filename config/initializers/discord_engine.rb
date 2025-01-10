5e02eed8203e7ac3d519ce3afac295b70025e45c
immprada <im.mprada@gmail.com>
2025-01-10 17:46:06 -0500

    setup DiscordEngine

---
+++ config/initializers/discord_engine.rb
@@ -0,0 +1,5 @@
+DiscordEngine.public_key = ENV['DISCORD_PUBLIC_KEY']
+DiscordEngine.bot_token = ENV['DISCORD_TOKEN']
+DiscordEngine.application_id = ENV['DISCORD_APP_ID']
+
+DiscordEngine.resolvers = []
