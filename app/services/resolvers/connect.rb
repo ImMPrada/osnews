740ddac34ce12e33f0fc352108c0e20b92b060a9
immprada <im.mprada@gmail.com>
2025-01-11 10:12:49 -0500

    add response for 'existing Guild' case

--- app/services/resolvers/connect.rb
+++ app/services/resolvers/connect.rb
@@ -3,21 +3,33 @@ module Resolvers
     attr_accessor :callback, :content
 
     def execute_action
-      create_guild
+      guild_already_exists_response and return if guild.present?
 
-      @callback = DiscordEngine::InteractionCallback.channel_message_with_source
-      @content = 'Hello this guild is now connected to osnews through this channel'
+      create_guild
+      new_guild_response
     end
 
     private
-
-    attr_accessor :guild
 
     def create_guild
       external_id = context.guild.id
       channel_id = context.channel_id
 
-      @guild = Guild.create!(external_id: external_id, channel_id: channel_id)
+      @guild = Guild.create!(external_id:, channel_id:)
+    end
+
+    def guild
+      @guild ||= Guild.find_by(external_id: context.guild.id, channel_id: context.channel_id)
+    end
+
+    def guild_already_exists_response
+      @callback = DiscordEngine::InteractionCallback.channel_message_with_source
+      @content = 'This guild is already connected to osnews through this channel'
+    end
+
+    def new_guild_response
+      @callback = DiscordEngine::InteractionCallback.channel_message_with_source
+      @content = 'Hello this guild is now connected to osnews through this channel'
     end
   end
 end
